90da5dd75598f8c3e9f5921824077004fbe31900
fatur <fatur@inforsys.co.id>
2018-12-31 09:19:51 +0700

    Tambah facade dan host

---
+++ ProxyApi/Controllers/HardwareController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Contracts;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Orleans;
+
+namespace ProxyApi.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class HardwareController : ControllerBase
+    {
+        private IClusterClient mOrleansClient;
+        public HardwareController(IClusterClient orleansClient)
+        {
+            mOrleansClient = orleansClient;
+        }
+        [HttpPut]
+        public async Task RequestMember(int hardwareId)
+        {
+            var proxy = this.mOrleansClient.GetGrain<IHardwareGrain>(hardwareId);
+            await proxy.RequestMember(30);
+        }
+        [HttpPut]
+        public async Task SendMember(int hardwareId,string memberNo, string memberName)
+        {
+            var proxy = this.mOrleansClient.GetGrain<IHardwareGrain>(hardwareId);
+            await proxy.SendMember(new Member { MemberNo = memberNo, Name = memberName });
+        }
+        [HttpGet]
+        public async Task<Member> GetMember(int hardwareId)
+        {
+            var proxy = this.mOrleansClient.GetGrain<IHardwareGrain>(hardwareId);
+            return await proxy.GetMember();
+        }
+        [HttpGet]
+        public async Task<ShareStatus> GetStatus(int hardwareId)
+        {
+            var proxy = this.mOrleansClient.GetGrain<IHardwareGrain>(hardwareId);
+            return await proxy.GetStatus();
+        }
+    }
+}
--- Silo/Program.cs
+++ Silo/Program.cs
@@ -1,12 +1,39 @@
-﻿using System;
+﻿using Microsoft.Extensions.Logging;
+using Orleans;
+using Orleans.Configuration;
+using Orleans.Hosting;
+using System;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Silo
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var siloBuilder = new SiloHostBuilder()
+            .UseLocalhostClustering()
+            .UseDashboard(options => { })
+            .Configure<ClusterOptions>(options =>
+            {
+                options.ClusterId = "ops";
+                options.ServiceId = "hardware.proxy";
+            })
+            .Configure<EndpointOptions>(options =>
+                options.AdvertisedIPAddress = IPAddress.Loopback)
+            .ConfigureLogging(logging => logging.AddConsole());
+
+            using (var host = siloBuilder.Build())
+            {
+                await host.StartAsync();
+
+                while (true)
+                {
+                    Thread.Sleep(10000);
+                }
+            }
         }
     }
 }
